7730fbabf6d280f4d1d93201ff5925b7a6fc62c8
Bryan Hundven <bryanhundven@gmail.com>
2012-08-12 15:28:45 -0700

    Add my own zip creator script

---
+++ mkzip.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+version="v1"
+output_dir=".."
+output_file="${output_dir}/unnamed-bhundven-edition-${version}.zip"
+
+echo -ne "Removing old zip file... "
+rm -f $output_file
+echo "Done!"
+
+echo -ne "Creating new zip file... "
+zip -q -r -9 $output_file data META-INF system tools updates
+if [ $? -eq 0 ]; then
+  echo "Zip failed!"
+  exit 1
+fi
+echo "Done!"
+
+echo "Finished!"
